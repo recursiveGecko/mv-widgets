139c2dd1b6bbc7b7907d752cfbdaad1e44a19070
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2023-07-26 21:13:44 +0200

    ci

--- build.zig
+++ build.zig
@@ -1,20 +1,16 @@
 const std = @import("std");
-const raylib = @import("lib/raylib/build.zig");
 const raygui = @import("lib/raygui/build.zig");
 
 const sep = std.fs.path.sep;
 
 inline fn projDir() []const u8 {
     const dir = comptime std.fs.path.dirname(@src().file) orelse unreachable;
-    return comptime dir ++ "/";
+    return comptime dir ++ .{sep};
 }
 
 pub fn build(b: *std.Build) !void {
     var target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    // const win_target = try std.zig.CrossTarget.parse(.{ .arch_os_abi = "x86_64-windows-gnu" });
-    // target = win_target;
 
     const name_suffix = b.option([]const u8, "filename-suffix", "Filename suffix") orelse "";
 
@@ -30,7 +26,7 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
-    raylib.addTo(b, exe, target, optimize);
+    addRaylibBindings(b, exe, target, optimize);
     raygui.addTo(b, exe, target, optimize);
     exe.addAnonymousModule(
         "mecha",
@@ -43,31 +39,63 @@ pub fn build(b: *std.Build) !void {
     exe.addAnonymousModule("primary_font.ttf", .{ .source_file = .{ .path = "data/Manrope-Regular.ttf" } });
     exe.addAnonymousModule("mono_font.ttf", .{ .source_file = .{ .path = "data/RobotoMono-Regular.ttf" } });
 
-    if (target.os_tag.? == .macos) {
-        std.debug.print("\n\n=> Adding macOS frameworks, libraries, and headers");
-        exe.addFrameworkPath("/System/Library/Frameworks");
-        exe.addSystemIncludePath("/System/Library/include");
-        exe.addLibraryPath("/System/Library/lib");
-    }
-
     b.installArtifact(exe);
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
+    // const run_cmd = b.addRunArtifact(exe);
+    // run_cmd.step.dependOn(b.getInstallStep());
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
+
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
+
+    // const unit_tests = b.addTest(.{
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
+    // const run_unit_tests = b.addRunArtifact(unit_tests);
+    // const test_step = b.step("test", "Run unit tests");
+    // test_step.dependOn(&run_unit_tests.step);
+}
+
+// Taken from raylib.zig's build.zig and modified to `addFrameworkPath` for cross-compiled MacOS builds
+pub fn addRaylibBindings(
+    b: *std.Build,
+    exe: *std.build.LibExeObjStep,
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.Mode,
+) void {
+    const raylib_upstream = @import("lib/raylib/raylib/src/build.zig");
+    const raylib_bindings_dir = projDir() ++ "lib/raylib/";
+    const raylib_upstream_dir = raylib_bindings_dir ++ "raylib/";
+
+    exe.addAnonymousModule("raylib", .{ .source_file = .{ .path = raylib_bindings_dir ++ "raylib.zig" } });
+    exe.addIncludePath(projDir() ++ "lib/raylib/raylib/src");
+    exe.addIncludePath(projDir() ++ "lib/raylib");
+
+    const bindings_lib = b.addStaticLibrary(.{ .name = "raylib-zig", .target = target, .optimize = optimize });
+    bindings_lib.addIncludePath(raylib_upstream_dir ++ "src");
+    bindings_lib.addIncludePath(raylib_bindings_dir);
+    bindings_lib.linkLibC();
+    bindings_lib.addCSourceFile(raylib_bindings_dir ++ "marshal.c", &.{});
+    exe.linkLibrary(bindings_lib);
+
+    const upstream_lib = raylib_upstream.addRaylib(b, target, optimize, .{});
+    exe.linkLibrary(upstream_lib);
+
+    b.installArtifact(bindings_lib);
+    b.installArtifact(upstream_lib);
+
+    switch (target.getOsTag()) {
+        .macos => {
+            upstream_lib.addFrameworkPath("/System/Library/Frameworks");
+            upstream_lib.addSystemIncludePath("/usr/include");
+            upstream_lib.addLibraryPath("/usr/lib");
+            exe.addFrameworkPath("/System/Library/Frameworks");
+        },
+        else => {},
     }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
 }
